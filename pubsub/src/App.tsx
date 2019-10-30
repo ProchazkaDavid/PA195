0d816c9ba5c093c05bcc728ddf6aed4e09e6620d
David Procházka <david@prochazka.dev>
2019-10-30 10:27:57 +0100

    FIX: ESlint formatting

--- pubsub/src/App.tsx
+++ pubsub/src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
           This is our glorious pubsub app 🚀
-        </p>
-      </header>
-    </div>
-  );
-}
+                </p>
+            </header>
+        </div>
+    );
+};
 
 export default App;
--- pubsub/src/api/index.tsx
+++ pubsub/src/api/index.tsx
@@ -3,12 +3,12 @@ import config from './config.json';
 
 const connect = () => {
     const client = Redis.createClient(config);
-    client.on("error", err => {
+    client.on('error', err => {
         console.log(`There has been a murder! - ${err}`);
-    })
+    });
 
-    client.set("key", "value", Redis.print);
-    client.get("key", value => {
+    client.set('key', 'value', Redis.print);
+    client.get('key', value => {
         console.log(`this is the value: ${value}`);
-    })
-}
+    });
+};
