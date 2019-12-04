3477793d1a5cedb2cab9f163b8952b79c67ca1ec
Josef Podaný <josef.podany@kiwi.com>
2019-12-04 16:26:59 +0100

    Restructure backend

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,18 +1,17 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(256) NOT NULL,
-    password VARCHAR(256) NOT NULL
-);
-
 CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
-    sender INTEGER REFERENCES users,
-    time_sent TIMESTAMP (6) WITH TIME ZONE,
-    channel SMALLINT NOT NULL CHECK ( channel between 0 and 9),
-    content VARCHAR(1024) NOT NULL
+  id SERIAL PRIMARY KEY,
+  sender VARCHAR(256) NOT NULL,
+  date VARCHAR(256),
+  room VARCHAR(256) NOT NULL,
+  text TEXT
 );
-
-grant INSERT, SELECT, UPDATE, DELETE ON users TO docker;
-grant INSERT, SELECT, UPDATE, DELETE ON messages TO docker;
-grant INSERT, SELECT, UPDATE, DELETE ON users_id_seq TO docker;
-grant INSERT, SELECT, UPDATE, DELETE ON messages_id_seq TO docker;
+grant
+INSERT,
+SELECT,
+UPDATE,
+  DELETE ON messages TO docker;
+grant
+INSERT,
+SELECT,
+UPDATE,
+  DELETE ON messages_id_seq TO docker;
