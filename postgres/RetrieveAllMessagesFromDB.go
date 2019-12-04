57ec231ddec2359a8b8b5a2038f18010db2135d4
Matej Tuzil <xtuzil@fi.muni.cz>
2019-12-04 12:37:41 +0100

    add func retrieveAllMessages()

--- postgres/RetrieveAllMessagesFromDB.go
+++ postgres/RetrieveAllMessagesFromDB.go
@@ -1,30 +1,10 @@
-package main
+package postgres
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
 
 	_ "github.com/lib/pq"
 )
-
-func getDBConnection() *sql.DB {
-	// Closing db instance left for caller. Call `defer db.Close()`
-	host := os.Getenv("PG_HOST")
-	port, _ := strconv.Atoi(os.Getenv("PG_PORT"))
-	user := os.Getenv("PG_USER")
-	password := os.Getenv("PG_PASSWORD")
-	dbname := os.Getenv("PG_DATABASE")
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
 
 type ChatMessage struct {
 	Id, Sender, Time_sent, Channel, Content string
