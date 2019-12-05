bcc2dd694ba3350384df7c262b1f17ac34e64a47
David Procházka <david@prochazka.dev>
2019-12-05 13:49:28 +0100

    FIX: simplified WS setup

--- pubsub/backend/server.go
+++ pubsub/backend/server.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 func serveWs(pool *Pool, w http.ResponseWriter, r *http.Request) {
-	conn, err := upgradeConnection(w, r)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Fprintf(w, "%+v\n", err)
+		log.Fatalln(err)
 	}
 
 	client := &Client{
